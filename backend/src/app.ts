86df66429b029c548082fa444ee5b0fbf19fa0a2
RohanKoriya <koriyarohan123@gmail.com>
2026-05-16 17:38:50 +0530

    Database setup and small fix

---
+++ backend/src/app.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
+app.get("/", (_req, res) => {
+  res.json({
+    success: true,
+    message: "API Running",
+  });
+});
+
+export default app;
