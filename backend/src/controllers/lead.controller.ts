db3fb3bdc8883602d74e190891f0528541d2988d
RohanKoriya <koriyarohan123@gmail.com>
2026-05-17 12:10:33 +0530

    added filtering search sorting and pagination

--- backend/src/controllers/lead.controller.ts
+++ backend/src/controllers/lead.controller.ts
@@ -22,15 +22,98 @@ export const createLead = async (
   }
 };
 
-export const getLeads = async (_req: Request, res: Response): Promise<void> => {
+export const getLeads = async (req: Request, res: Response): Promise<void> => {
   try {
-    const leads = await Lead.find().sort({
-      createdAt: -1,
-    });
+    const { status, source, search, sort = "latest", page = "1" } = req.query;
+
+    const query: Record<string, unknown> = {};
+
+    /*
+      STATUS FILTER
+    */
+    if (status) {
+      query.status = status;
+    }
+
+    /*
+      SOURCE FILTER
+    */
+    if (source) {
+      query.source = source;
+    }
+
+    /*
+      SEARCH FILTER
+      Search by:
+      - name
+      - email
+    */
+    if (search) {
+      query.$or = [
+        {
+          name: {
+            $regex: search,
+            $options: "i",
+          },
+        },
+        {
+          email: {
+            $regex: search,
+            $options: "i",
+          },
+        },
+      ];
+    }
+
+    /*
+      SORTING
+    */
+    let sortOption = {};
+
+    if (sort === "latest") {
+      sortOption = { createdAt: -1 };
+    }
+
+    if (sort === "oldest") {
+      sortOption = { createdAt: 1 };
+    }
+
+    /*
+      PAGINATION
+    */
+    const pageNumber = parseInt(page as string) || 1;
+
+    const limit = 10;
+
+    const skip = (pageNumber - 1) * limit;
+
+    /*
+      TOTAL RECORDS
+    */
+    const totalRecords = await Lead.countDocuments(query);
+
+    /*
+      FETCH DATA
+    */
+    const leads = await Lead.find(query)
+      .sort(sortOption)
+      .skip(skip)
+      .limit(limit);
+
+    /*
+      TOTAL PAGES
+    */
+    const totalPages = Math.ceil(totalRecords / limit);
 
     res.status(200).json({
       success: true,
-      count: leads.length,
+
+      pagination: {
+        currentPage: pageNumber,
+        totalPages,
+        totalRecords,
+      },
+
       data: leads,
     });
   } catch (error) {
